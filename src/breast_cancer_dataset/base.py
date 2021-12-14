[FIX] Image segmentation data augmentations via data generator [FIX] Refactor frozen layers cnn. Batch normalization layers freezed on clasification task. [NEW] Ratio padding to images. Insert black pixels to fit specified ratio.

src/breast_cancer_dataset/base.py
@@ -1,13 +1,16 @@
-
 import os
 import tqdm
+import cv2
 
 import pandas as pd
+import numpy as np
 
 from typing import io, List
 from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
 from tqdm import tqdm
+from tensorflow.keras.utils import Sequence
+from albumentations import Compose
 
 from src.preprocessing.image_conversion import convert_img
 from src.preprocessing.image_processing import full_image_pipeline
@@ -249,3 +252,72 @@ class GeneralDataBase:
         # Se eliminan las imagenes que no hayan podido ser procesadas y se obtiene un único registro a nivel de label
         # e imagen.
         self.delete_observations()
+
+
+class Dataset:
+
+    def __init__(self, df: pd.DataFrame, img_col: str, mask_col: str, augmentation: Compose = None):
+
+        assert img_col in df.columns, f'{img_col} not in df'
+        assert mask_col in df.columns, f'{mask_col} not in df'
+
+        self.df = df
+        self.img_col = img_col
+        self.mask_col = mask_col
+        self.augmentation = augmentation
+
+    def __getitem__(self, i):
+
+        # read data
+        image = cv2.cvtColor(cv2.imread(self.df.iloc[i][self.img_col]), cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.df.iloc[i][self.mask_col], cv2.IMREAD_GRAYSCALE)
+
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.df)
+
+
+class Dataloder(Sequence):
+    """Load data from dataset and form batches
+
+    Args:
+        dataset: instance of Dataset class for image loading and preprocessing.
+        batch_size: Integet number of images in batch.
+        shuffle: Boolean, if `True` shuffle image indexes each epoch.
+    """
+
+    def __init__(self, dataset, batch_size=1, shuffle=False, seed=0):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indexes = np.arange(len(dataset))
+        self.seed = seed
+
+        self.on_epoch_end()
+
+    def __getitem__(self, i):
+
+        # collect batch data
+        data = []
+        for j in range(i * self.batch_size, (i + 1) * self.batch_size):
+            data.append(self.dataset[j])
+
+        # transpose list of lists
+        batch = [np.stack(samples, axis=0) for samples in zip(*data)]
+
+        return batch
+
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return len(self.indexes) // self.batch_size
+
+    def on_epoch_end(self):
+        """Callback function to shuffle indexes each epoch"""
+        if self.shuffle:
+            self.indexes = np.random.RandomState(seed=self.seed).permutation(self.indexes)

src/breast_cancer_dataset/database_generator.py
@@ -2,13 +2,16 @@ import json
 import logging
 import os
 
+import cv2
 import pandas as pd
 import numpy as np
 
 from typing import Callable
+from albumentations import Lambda, Compose
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, Iterator
 from sklearn.model_selection import train_test_split
 
+from breast_cancer_dataset.base import Dataset, Dataloder
 from src.breast_cancer_dataset.databases.cbis_ddsm import DatasetCBISDDSM, DatasetCBISDDSMCrop
 from src.breast_cancer_dataset.databases.inbreast import DatasetINBreast, DatasetINBreastCrop
 from src.breast_cancer_dataset.databases.mias import DatasetMIAS, DatasetMIASCrop
@@ -16,13 +19,14 @@ from src.utils.config import (
     MODEL_FILES, SEED, CLASSIFICATION_DATA_AUGMENTATION_FUNCS, TRAIN_DATA_PROP, PREPROCESSING_FUNCS,
     PREPROCESSING_CONFIG, EXPERIMENT, IMG_SHAPE, SEGMENTATION_DATA_AUGMENTATION_FUNCS, PATCH_SIZE
 )
+from src.preprocessing.image_processing import resize_img
 
 
 class BreastCancerDataset:
 
     DBS = {
-            'COMPLETE_IMAGE': [DatasetCBISDDSM, DatasetMIAS, DatasetINBreast],
-            'PATCHES': [DatasetCBISDDSMCrop, DatasetMIASCrop, DatasetINBreastCrop]
+            'COMPLETE_IMAGE': [DatasetCBISDDSM, DatasetINBreast],
+            'PATCHES': [DatasetCBISDDSMCrop , DatasetINBreastCrop]
         }
 
     def __init__(self, get_class: bool = True, split_data: bool = True, excel_path: str = ''):
@@ -137,72 +141,49 @@ class BreastCancerDataset:
         # usuario mediante input, se utilizará la técnica de interpolación lanzcos. Por otra parte, para generar una
         # salida one hot encoding en función de la clase de cada muestra, se parametriza class_mode como 'categorical'.
 
-        def image_mask_generator(image_data_generator: Iterator, mask_data_generator: Iterator):
-            for (img, mask) in zip(image_data_generator, mask_data_generator):
-                yield (img, mask)
+        train_aug = Compose([
+            *list(SEGMENTATION_DATA_AUGMENTATION_FUNCS.values()),
+            Lambda(
+                image=lambda x, **kgs: resize_img(x, height=size[0], width=size[1], interpolation=cv2.INTER_LANCZOS4),
+                mask=lambda x, **kgs: resize_img(x, height=size[0], width=size[1], interpolation=cv2.INTER_NEAREST),
+                name='image resizing'
+            ),
+            Lambda(mask=lambda x, **kwargs: np.float32(x.round().clip(0, 1)), name='normalize mask'),
+            Lambda(image=callback, name='cnn processing function')
+        ])
 
-        img_params = dict(
-            target_size=size,
-            interpolation='lanczos',
-            shufle=True,
-            seed=SEED,
-            batch_size=batch_size,
-            class_mode=None,
-            directory=None,
-        )
-
-        mask_params = dict(
-            target_size=size,
-            interpolation='nearest',
-            shufle=True,
-            seed=SEED,
-            batch_size=batch_size,
-            class_mode=None,
-            directory=None,
-            color_mode='grayscale',
-        )
-
-        # Parametrización del generador de entrenamiento. Las imagenes de entrenamiento recibirán un conjunto de
-        # modificaciones aleatorias con el objetivo de aumentar el set de datos de entrenamiento y evitar de esta forma
-        # el over fitting.
-        train_datagen_img = ImageDataGenerator(**SEGMENTATION_DATA_AUGMENTATION_FUNCS, preprocessing_function=callback)
-        train_datagen_mask = ImageDataGenerator(**SEGMENTATION_DATA_AUGMENTATION_FUNCS, rescale=1. / 255)
-
-        # Parametrización del generador de validación. Las imagenes de validación exclusivamente se les aplicará la
-        # técnica de preprocesado subministrada por el usuario.
-        val_datagen_img = ImageDataGenerator(preprocessing_function=callback)
-        val_datagen_mask = ImageDataGenerator(rescale=1. / 255)
+        val_aug = Compose([
+            Lambda(
+                image=lambda x, **kgs: resize_img(x, height=size[0], width=size[1], interpolation=cv2.INTER_LANCZOS4),
+                mask=lambda x, **kgs: resize_img(x, height=size[0], width=size[1], interpolation=cv2.INTER_NEAREST),
+                name='image resizing',
+            ),
+            Lambda(mask=lambda x, **kwargs: np.float32(x.round().clip(0, 1)), name='normalize mask'),
+            Lambda(image=callback, name='cnn processing function')
+        ])
 
         # Para evitar entrecruzamientos de imagenes entre train y validación a partir del atributo shuffle=True, cada
         # generador se aplicará sobre una muestra disjunta del set de datos representada mediante la columna dataset.
 
         # Se chequea que existen observaciones de entrenamiento para poder crear el dataframeiterator.
         if len(self.df[self.df.TRAIN_VAL == 'train']) == 0:
-            train_df_iter_img, train_df_iter_mask = None, None
+            train_dataloader = None
             logging.warning('No existen registros para generar un generador de train. Se retornará None')
         else:
-            train_df_iter_img = train_datagen_img.flow_from_dataframe(
-                dataframe=self.df[self.df.TRAIN_VAL == 'train'], x_col='PROCESSED_IMG', **img_params
-            )
-            train_df_iter_mask = train_datagen_mask.flow_from_dataframe(
-                dataframe=self.df[self.df.TRAIN_VAL == 'train'], x_col='PROCESSED_MASK', **mask_params
-            )
+            # Dataset for train images
+            train_df = Dataset(self.df[self.df.TRAIN_VAL == 'train'], 'PROCESSED_IMG', 'PROCESSED_MASK', train_aug)
+            train_dataloader = Dataloder(train_df, batch_size=batch_size, shuffle=True, seed=SEED)
 
         # Se chequea que existen observaciones de validación para poder crear el dataframeiterator.
         if len(self.df[self.df.TRAIN_VAL == 'val']) == 0:
-            val_df_iter_img = None
-            val_df_iter_mask = None
+            valid_dataloader = None
             logging.warning('No existen registros para generar un generador de validación. Se retornará None')
         else:
-            val_df_iter_img = val_datagen_img.flow_from_dataframe(
-                dataframe=self.df[self.df.TRAIN_VAL == 'val'], x_col='PROCESSED_IMG', **img_params
-            )
-            val_df_iter_mask = val_datagen_mask.flow_from_dataframe(
-                dataframe=self.df[self.df.TRAIN_VAL == 'val'], x_col='PROCESSED_MASK', **mask_params
-            )
+            # Dataset for validation images
+            val_df = Dataset(self.df[self.df.TRAIN_VAL == 'val'], 'PROCESSED_IMG', 'PROCESSED_MASK', val_aug)
+            valid_dataloader = Dataloder(val_df, batch_size=batch_size, shuffle=True, seed=SEED)
 
-        return image_mask_generator(train_df_iter_img, train_df_iter_mask), \
-               image_mask_generator(val_df_iter_img, val_df_iter_mask)
+        return train_dataloader, valid_dataloader
 
     def split_dataset(self, train_prop: float, stratify: bool = True):
         """

src/cnns/classification.py
@@ -3,21 +3,19 @@ import numpy as np
 from typing import Callable, io, Union, Tuple
 from time import process_time
 
-from segmentation_models.models.unet import Unet
-from segmentation_models.metrics import iou_score
-from segmentation_models.losses import dice_loss
 from tensorflow.keras import Model, Sequential, optimizers, callbacks
 from tensorflow.keras.backend import count_params
 from tensorflow.keras.callbacks import History
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import L2
 from tensorflow.keras.applications import resnet50, densenet, vgg16, inception_v3
-from tensorflow.python.keras.constraints import maxnorm
 from tensorflow.python.keras.preprocessing.image import DataFrameIterator
-from tensorflow.keras.layers import Conv2D, Dropout, MaxPooling2D, Dense, GlobalAveragePooling2D, Input
-from tensorflow.keras.losses import CategoricalCrossentropy, BinaryCrossentropy
+from tensorflow.keras.layers import (
+    Conv2D, Dropout, MaxPooling2D, Dense, GlobalAveragePooling2D, Input, BatchNormalization
+)
 
 from src.utils.functions import get_path, get_number_of_neurons
+from src.utils.config import CLASSIFICATION_LOSS
 
 
 class GeneralModel:
@@ -25,7 +23,7 @@ class GeneralModel:
     __name__ = 'GeneralModel'
     model: Model = None
     callbakcs = {}
-    loss = CategoricalCrossentropy()
+    loss = CLASSIFICATION_LOSS
     metrics = ['accuracy']
     history: History = None
     shape = (224, 224, 3)
@@ -102,12 +100,16 @@ class GeneralModel:
             train_layers = \
                 [l for layers in list_keys[:list_keys.index(unfrozen_layers) + 1] for l in self.LAYERS_DICT[layers]]
             for layer in train_layers:
-                self.baseline.get_layer(layer).trainable = True
+                try:
+                    assert not isinstance(self.baseline.get_layer(layer), BatchNormalization)
+                    self.baseline.get_layer(layer).trainable = True
+                except AssertionError:
+                    self.baseline.get_layer(layer).trainable = False
 
         else:
             raise ValueError(f'Unfrozen layers parametrization for {unfrozen_layers}')
 
-    def start_train(self, train_data: DataFrameIterator, val_data: DataFrameIterator, epochs: int, batch_size: int,
+    def start_train(self, train_data: DataFrameIterator, val_data: DataFrameIterator, epochs: int,
                     opt: optimizers = Adam(1e-3), unfrozen_layers: str = 'ALL') -> Tuple[float, int]:
         """
         Función que compila el modelo, añade los callbacks definidos por el usuario y entrena el modelo
@@ -142,29 +144,28 @@ class GeneralModel:
     def register_callback(self, **kargs: callbacks):
         self.callbakcs = {**self.callbakcs, **kargs}
 
-    def train_from_scratch(self, train_data, val_data, epochs: int, batch_size: int, opt: optimizers = None):
+    def train_from_scratch(self, train_data, val_data, epochs: int, opt: optimizers = None):
         """
             Función utilizada para entrenar completamente el modelo.
         """
-        t, e = self.start_train(train_data, val_data, epochs, batch_size, opt, unfrozen_layers='ALL')
+        t, e = self.start_train(train_data, val_data, epochs, opt, unfrozen_layers='ALL')
         return t, e
 
-    def extract_features(self, train_data, val_data, epochs: int, batch_size: int, opt: optimizers = None):
+    def extract_features(self, train_data, val_data, epochs: int, opt: optimizers = None):
         """
         Función utilizada para aplicar un proceso de extract features de modo que se conjela la arquitectura definida en
         self.baseline y se entrenan las últimas capas de la arquitectura
         """
-        t, e = self.start_train(train_data, val_data, epochs, batch_size, opt, unfrozen_layers='0FT')
+        t, e = self.start_train(train_data, val_data, epochs, opt, unfrozen_layers='0FT')
         return t, e
 
-    def fine_tunning(self, train_data, val_data, epochs: int, batch_size: int, opt: optimizers = None,
-                     unfrozen_layers: str = '1FT'):
+    def fine_tunning(self, train_data, val_data, epochs: int, opt: optimizers = None, unfrozen_layers: str = '1FT'):
         """
         Función utilizada para aplicar un proceso de transfer learning de modo que se conjelan n - k capas. Las k capas
         entrenables en la arquitectura definida por self.baseline se determinarán a partir del método
         set_trainable_layers
         """
-        t, e = self.start_train(train_data, val_data, epochs, batch_size, opt, unfrozen_layers=unfrozen_layers)
+        t, e = self.start_train(train_data, val_data, epochs, opt, unfrozen_layers=unfrozen_layers)
         return t, e
 
     def save_model(self, dirname: str, model_name: str):
@@ -185,7 +186,9 @@ class GeneralModel:
         return len([l for l in self.baseline.layers if l.trainable])
 
     def get_trainable_params(self) -> int:
-        return int(np.sum([count_params(p) for p in self.baseline.trainable_weights]))
+        return int(np.sum([
+            count_params(p) for lay in self.baseline.layers for p in lay.trainable_weights if lay.trainable
+        ]))
 
 
 class VGG16Model(GeneralModel):
@@ -193,10 +196,10 @@ class VGG16Model(GeneralModel):
     __name__ = 'VGG16'
     LAYERS_DICT = {
         '0FT': [],
-        '1FT': ['block5_conv1', 'block5_conv2', 'block5_conv3', 'block5_pool'],
-        '2FT': ['block4_conv1', 'block4_conv2', 'block4_conv3', 'block4_pool'],
-        '3FT': ['block3_conv1', 'block3_conv2', 'block3_conv3', 'block3_pool'],
-        '4FT': ['block2_conv1', 'block2_conv2', 'block2_pool']
+        '1FT': ['block5_conv1', 'block5_conv2', 'block5_conv3'],
+        '2FT': ['block4_conv1', 'block4_conv2', 'block4_conv3'],
+        '3FT': ['block3_conv1', 'block3_conv2', 'block3_conv3'],
+        '4FT': ['block2_conv1', 'block2_conv2']
     }
 
     def __init__(self, n: int, weights: Union[str, io] = None):
@@ -212,24 +215,27 @@ class Resnet50Model(GeneralModel):
     LAYERS_DICT = {
         '0FT': [],
         '1FT': [
-            'conv5_block3_1_conv', 'conv5_block3_1_bn', 'conv5_block3_1_relu', 'conv5_block3_2_conv',
-            'conv5_block3_2_bn', 'conv5_block3_2_relu', 'conv5_block3_3_conv', 'conv5_block3_3_bn', 'conv5_block3_add',
-            'conv5_block3_out'
+            'conv5_block3_1_conv', 'conv5_block3_2_conv', 'conv5_block3_3_conv', 'conv5_block2_1_conv',
+            'conv5_block2_2_conv', 'conv5_block2_3_conv', 'conv5_block1_1_conv', 'conv5_block1_2_conv',
+            'conv5_block1_0_conv', 'conv5_block1_3_conv'
         ],
         '2FT': [
-            'conv5_block2_1_conv', 'conv5_block2_1_bn', 'conv5_block2_1_relu', 'conv5_block2_2_conv',
-            'conv5_block2_2_bn', 'conv5_block2_2_relu', 'conv5_block2_3_conv', 'conv5_block2_3_bn', 'conv5_block2_add',
-            'conv5_block2_out'
+            'conv4_block1_1_conv', 'conv4_block1_2_conv', 'conv4_block1_0_conv', 'conv4_block1_3_conv',
+            'conv4_block2_1_conv', 'conv4_block2_2_conv', 'conv4_block2_3_conv', 'conv4_block3_1_conv',
+            'conv4_block3_2_conv', 'conv4_block3_3_conv', 'conv4_block4_1_conv', 'conv4_block4_2_conv',
+            'conv4_block4_3_conv', 'conv4_block5_1_conv', 'conv4_block5_2_conv', 'conv4_block5_3_conv',
+            'conv4_block6_1_conv', 'conv4_block6_2_conv', 'conv4_block6_3_conv'
         ],
         '3FT': [
-            'conv5_block1_1_conv', 'conv5_block1_1_bn', 'conv5_block1_1_relu', 'conv5_block1_2_conv',
-            'conv5_block1_2_bn', 'conv5_block1_2_relu', 'conv5_block1_0_conv', 'conv5_block1_3_conv',
-            'conv5_block1_0_bn', 'conv5_block1_3_bn', 'conv5_block1_add', 'conv5_block1_out'
+            'conv3_block1_1_conv', 'conv3_block1_2_conv', 'conv3_block1_0_conv', 'conv3_block1_3_conv',
+            'conv3_block2_1_conv', 'conv3_block2_2_conv', 'conv3_block2_3_conv', 'conv3_block3_1_conv',
+            'conv3_block3_2_conv', 'conv3_block3_3_conv', 'conv3_block4_1_conv', 'conv3_block4_2_conv',
+            'conv3_block4_3_conv'
         ],
         '4FT': [
-            'conv4_block6_1_conv', 'conv4_block6_1_bn', 'conv4_block6_1_relu', 'conv4_block6_2_conv',
-            'conv4_block6_2_bn', 'conv4_block6_2_relu', 'conv4_block6_3_conv', 'conv4_block6_3_bn', 'conv4_block6_add',
-            'conv4_block6_out'
+            'conv2_block1_1_conv', 'conv2_block1_2_conv', 'conv2_block1_0_conv', 'conv2_block1_3_conv',
+            'conv2_block2_1_conv', 'conv2_block2_2_conv', 'conv2_block2_3_conv', 'conv2_block3_1_conv',
+            'conv2_block3_2_conv', 'conv2_block3_3_conv'
         ]
     }
     shape = (224, 224, 3)
@@ -247,34 +253,19 @@ class InceptionV3Model(GeneralModel):
     LAYERS_DICT = {
         '0FT': [],
         '1FT': [
-            'conv2d_89', 'batch_normalization_89', 'activation_89', 'conv2d_90', 'batch_normalization_90',
-            'activation_90', 'conv2d_91', 'batch_normalization_91', 'activation_91', 'concatenate_1',
-            'conv2d_92', 'batch_normalization_92', 'activation_92', 'conv2d_86', 'batch_normalization_86',
-            'activation_86', 'conv2d_87', 'batch_normalization_87',  'activation_87', 'mixed9_1', 'conv2d_88',
-            'batch_normalization_88', 'activation_88', 'average_pooling2d_8', 'conv2d_93', 'batch_normalization_93',
-            'activation_93', 'conv2d_85', 'batch_normalization_85', 'activation_85', 'mixed10'
+            'conv2d_89', 'conv2d_90', 'conv2d_91', 'conv2d_92', 'conv2d_86', 'conv2d_87', 'conv2d_88', 'conv2d_93',
+            'conv2d_85'
         ],
         '2FT': [
-            'conv2d_80', 'batch_normalization_80', 'activation_80', 'conv2d_81', 'batch_normalization_81',
-            'activation_81', 'conv2d_82', 'batch_normalization_82', 'activation_82', 'concatenate',
-            'conv2d_83', 'batch_normalization_83', 'activation_83', 'conv2d_77', 'batch_normalization_77',
-            'activation_77', 'conv2d_78', 'batch_normalization_78',  'activation_78', 'mixed9_0', 'conv2d_79',
-            'batch_normalization_79', 'activation_79', 'average_pooling2d_7', 'conv2d_84', 'batch_normalization_84',
-            'activation_84', 'conv2d_76', 'batch_normalization_76', 'activation_76', 'mixed9'
+            'conv2d_80', 'conv2d_81', 'conv2d_82', 'conv2d_83', 'conv2d_77', 'conv2d_78', 'conv2d_79', 'conv2d_84',
+            'conv2d_76',
         ],
         '3FT': [
-            'conv2d_72', 'batch_normalization_72', 'activation_72', 'conv2d_73', 'batch_normalization_73',
-            'activation_73', 'conv2d_74', 'batch_normalization_74', 'activation_74', 'conv2d_75',
-            'batch_normalization_75', 'activation_75', 'conv2d_70', 'batch_normalization_70', 'activation_70',
-            'conv2d_71', 'batch_normalization_71', 'activation_71', 'max_pooling2d_3', 'mixed8'
+            'conv2d_72', 'conv2d_73', 'conv2d_74', 'conv2d_75', 'conv2d_70', 'conv2d_71'
         ],
         '4FT': [
-            'conv2d_64', 'batch_normalization_64', 'activation_64', 'conv2d_65', 'batch_normalization_65',
-            'activation_65', 'conv2d_66', 'batch_normalization_66', 'activation_66', 'conv2d_67',
-            'batch_normalization_67', 'activation_67', 'conv2d_68', 'batch_normalization_68', 'activation_68',
-            'conv2d_61', 'batch_normalization_61', 'activation_61', 'conv2d_62', 'batch_normalization_62',
-            'activation_62', 'conv2d_63', 'batch_normalization_63', 'activation_63', 'average_pooling2d_6', 'conv2d_69',
-            'batch_normalization_69', 'activation_69', 'conv2d_60', 'batch_normalization_60', 'activation_60', 'mixed7'
+            'conv2d_64', 'conv2d_65', 'conv2d_66', 'conv2d_67', 'conv2d_68', 'conv2d_61', 'conv2d_62', 'conv2d_63',
+            'conv2d_69', 'conv2d_60',
         ]
     }
     shape = (299, 299, 3)
@@ -291,20 +282,41 @@ class DenseNetModel(GeneralModel):
     LAYERS_DICT = {
         '0FT': [],
         '1FT': [
-            'conv5_block16_0_bn', 'conv5_block16_0_relu', 'conv5_block16_1_conv', 'conv5_block16_1_bn',
-            'conv5_block16_1_relu', 'conv5_block16_2_conv', 'conv5_block16_concat', 'bn', 'relu'
+            'conv5_block1_1_conv', 'conv5_block1_2_conv', 'conv5_block2_1_conv', 'conv5_block2_2_conv',
+            'conv5_block3_1_conv', 'conv5_block3_2_conv', 'conv5_block4_1_conv', 'conv5_block4_2_conv',
+            'conv5_block5_1_conv', 'conv5_block5_2_conv', 'conv5_block6_1_conv', 'conv5_block6_2_conv',
+            'conv5_block7_1_conv', 'conv5_block7_2_conv', 'conv5_block8_1_conv', 'conv5_block8_2_conv',
+            'conv5_block9_1_conv', 'conv5_block9_2_conv', 'conv5_block10_1_conv', 'conv5_block10_2_conv',
+            'conv5_block11_1_conv', 'conv5_block11_2_conv', 'conv5_block12_1_conv', 'conv5_block12_2_conv',
+            'conv5_block13_1_conv', 'conv5_block13_2_conv', 'conv5_block14_1_conv', 'conv5_block14_2_conv',
+            'conv5_block15_1_conv', 'conv5_block15_2_conv', 'conv5_block16_1_conv', 'conv5_block16_2_conv'
         ],
         '2FT': [
-            'conv5_block15_0_bn', 'conv5_block15_0_relu', 'conv5_block15_1_conv', 'conv5_block15_1_bn',
-            'conv5_block15_1_relu', 'conv5_block15_2_conv', 'conv5_block15_concat'
+            'conv4_block1_1_conv', 'conv4_block1_2_conv', 'conv4_block2_1_conv', 'conv4_block2_2_conv',
+            'conv4_block3_1_conv', 'conv4_block3_2_conv', 'conv4_block4_1_conv', 'conv4_block4_2_conv',
+            'conv4_block5_1_conv', 'conv4_block5_2_conv', 'conv4_block6_1_conv', 'conv4_block6_2_conv',
+            'conv4_block7_1_conv', 'conv4_block7_2_conv', 'conv4_block8_1_conv', 'conv4_block8_2_conv',
+            'conv4_block9_1_conv', 'conv4_block9_2_conv', 'conv4_block10_1_conv', 'conv4_block10_2_conv',
+            'conv4_block11_1_conv', 'conv4_block11_2_conv', 'conv4_block12_1_conv', 'conv4_block12_2_conv',
+            'conv4_block13_1_conv', 'conv4_block13_2_conv', 'conv4_block14_1_conv', 'conv4_block14_2_conv',
+            'conv4_block15_1_conv', 'conv4_block15_2_conv', 'conv4_block16_1_conv', 'conv4_block16_2_conv',
+            'conv4_block17_1_conv', 'conv4_block17_2_conv', 'conv4_block18_1_conv', 'conv4_block18_2_conv',
+            'conv4_block19_1_conv', 'conv4_block19_2_conv', 'conv4_block20_1_conv', 'conv4_block20_2_conv',
+            'conv4_block21_1_conv', 'conv4_block21_2_conv', 'conv4_block22_1_conv', 'conv4_block22_2_conv',
+            'conv4_block23_1_conv', 'conv4_block23_2_conv', 'conv4_block24_1_conv', 'conv4_block24_2_conv'
         ],
         '3FT': [
-            'conv5_block14_0_bn', 'conv5_block14_0_relu', 'conv5_block14_1_conv', 'conv5_block14_1_bn',
-            'conv5_block14_1_relu', 'conv5_block14_2_conv', 'conv5_block14_concat'
+            'conv3_block1_1_conv', 'conv3_block1_2_conv', 'conv3_block2_1_conv', 'conv3_block2_2_conv',
+            'conv3_block3_1_conv', 'conv3_block3_2_conv', 'conv3_block4_1_conv', 'conv3_block4_2_conv',
+            'conv3_block5_1_conv', 'conv3_block5_2_conv', 'conv3_block6_1_conv', 'conv3_block6_2_conv',
+            'conv3_block7_1_conv', 'conv3_block7_2_conv', 'conv3_block8_1_conv', 'conv3_block8_2_conv',
+            'conv3_block9_1_conv', 'conv3_block9_2_conv', 'conv3_block10_1_conv', 'conv3_block10_2_conv',
+            'conv3_block11_1_conv', 'conv3_block11_2_conv', 'conv3_block12_1_conv', 'conv3_block12_2_conv'
         ],
         '4FT': [
-            'conv5_block13_0_bn', 'conv5_block13_0_relu', 'conv5_block13_1_conv', 'conv5_block13_1_bn',
-            'conv5_block13_1_relu', 'conv5_block13_2_conv', 'conv5_block13_concat'
+            'conv2_block1_1_conv', 'conv2_block1_2_conv', 'conv2_block2_1_conv', 'conv2_block2_2_conv',
+            'conv2_block3_1_conv', 'conv2_block3_2_conv', 'conv2_block4_1_conv', 'conv2_block4_2_conv',
+            'conv2_block5_1_conv', 'conv2_block5_2_conv', 'conv2_block6_1_conv', 'conv2_block6_2_conv'
         ]
     }
 
@@ -313,75 +325,3 @@ class DenseNetModel(GeneralModel):
             n=n, baseline=densenet.DenseNet121(include_top=False, weights=weights, input_shape=self.shape),
             preprocess_func=densenet.preprocess_input
         )
-
-
-class UnetVGG16Model(GeneralModel):
-
-    __name__ = 'UnetVGG16'
-    LAYERS_DICT = VGG16Model.LAYERS_DICT
-    loss = dice_loss
-    metrics = [iou_score]
-
-    def __init__(self, weights: Union[str, io] = None):
-        from segmentation_models import Unet
-        super(UnetVGG16Model, self).__init__(
-            n=0, baseline=Unet('vgg16', encoder_weights=weights, classes=1, encoder_freeze=True, activation='sigmoid'),
-            preprocess_func=vgg16.preprocess_input
-        )
-
-    def create_model(self):
-        self.model = self.baseline
-
-
-class UnetResnet50Model(GeneralModel):
-
-    __name__ = 'UnetResnet50'
-    LAYERS_DICT = Resnet50Model.LAYERS_DICT
-    loss = dice_loss
-    metrics = [iou_score]
-
-    def __init__(self, weights: Union[str, io] = None):
-        from segmentation_models import Unet
-        super(UnetResnet50Model, self).__init__(
-            n=0, baseline=Unet('resnet50', encoder_weights=weights, classes=1, encoder_freeze=True, activation='sigmoid'),
-            preprocess_func=resnet50.preprocess_input
-        )
-
-    def create_model(self):
-        self.model = self.baseline
-
-
-class UnetDenseNetModel(GeneralModel):
-
-    __name__ = 'UnetDenseNet'
-    LAYERS_DICT = DenseNetModel.LAYERS_DICT
-    loss = dice_loss
-    metrics = [iou_score]
-
-    def __init__(self, weights: Union[str, io] = None):
-        from segmentation_models import Unet
-        super(UnetDenseNetModel, self).__init__(
-            baseline=Unet('densenet121', encoder_weights=weights, classes=1, encoder_freeze=True, activation='sigmoid'),
-            preprocess_func=densenet.preprocess_input
-        )
-
-    def create_model(self):
-        self.model = self.baseline
-
-
-class UnetInceptionV3Model(InceptionV3Model, GeneralModel):
-
-    __name__ = 'UnetInceptionV3'
-    LAYERS_DICT = InceptionV3Model.LAYERS_DICT
-    loss = dice_loss
-    metrics = [iou_score]
-
-    def __init__(self, weights: Union[str, io] = None):
-        from segmentation_models import Unet
-        super(UnetInceptionV3Model, self).__init__(
-            baseline=Unet('inceptionv3', encoder_weights=weights, classes=1, encoder_freeze=True, activation='sigmoid'),
-            preprocess_func=inception_v3.preprocess_input
-        )
-
-    def create_model(self):
-        self.model = self.baseline

src/preprocessing/functions.py
@@ -250,31 +250,31 @@ def apply_clahe_transform(img: np.ndarray, mask: np.ndarray = None, clip: int = 
 
 
 @detect_func_err
-def pad_image_into_square(img: np.ndarray) -> np.ndarray:
+def pad_image_into_square(img: np.ndarray, ratio: str = '1:2') -> np.ndarray:
     """
 
     :param img:
     :return:
     """
+    height_ratio, width_ratio = map(int, ratio.split(':'))
+
+    desired_width = img.shape[0] * height_ratio // width_ratio
+    desired_height = img.shape[1] * width_ratio // height_ratio
+    padded_img = np.zeros(shape=(max(img.shape[0], desired_height), max(desired_width, img.shape[1])), dtype=np.uint8)
 
     if len(img.shape) == 2:
         nrows, ncols = img.shape
-
-        padded_img = np.zeros(shape=(max(nrows, ncols), max(nrows, ncols)), dtype=np.uint8)
         padded_img[:nrows, :ncols] = img
 
-        return padded_img
     else:
         nrows, ncols, d = img.shape
-
-        padded_img = np.zeros(shape=(max(nrows, ncols), max(nrows, ncols), d), dtype=np.uint8)
         padded_img[:nrows, :ncols, :] = img
 
-        return padded_img
+    return padded_img
 
 
 @detect_func_err
-def resize_img(img: np.ndarray, size: tuple = (300, 300)) -> np.ndarray:
+def resize_img(img: np.ndarray, height: int, width: int, interpolation=cv2.INTER_LANCZOS4) -> np.ndarray:
     """
 
     :param img:
@@ -282,7 +282,7 @@ def resize_img(img: np.ndarray, size: tuple = (300, 300)) -> np.ndarray:
     :return:
     """
 
-    return cv2.resize(src=img.copy(), dsize=size, interpolation=cv2.INTER_LANCZOS4)
+    return cv2.resize(src=img.copy(), dsize=(width, height), interpolation=interpolation)
 
 
 @detect_func_err