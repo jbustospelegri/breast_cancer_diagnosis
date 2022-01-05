81519627a9030a88c6d16783175d44a6c739c57a
jbustos <jbustospelegri@gmail.com>
2022-01-05 20:23:42 +0100

    [NEW] Readme.md informed [NEW] Comentarios modulo algorithms

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Este proyecto persigue el objetivo de crear una herramienta que permita clasific
 presentes en imágenes mamográficas como benignas o malignas.
 
 Para la realización de esta aplicación se han utilizado 4 arquitecturas que componen el estado 
-del arte en tareas de clasificación de imagen, como son: _VGG16_, _ResNet50_, _DenseNet121_ y _InceptionV3_.
+del arte en tareas de clasificación de imagen como son: _VGG16_, _ResNet50_, _DenseNet121_ y _InceptionV3_.
 Las predicciones realizadas por cada arquitectura son combinadas en un _Random Forest_ para obtener la
-predicción final de cada instancia (probabilidad de que un cáncer sea maligno).
+predicción final de cada instancia (probabilidad de que un cáncer sea maligno)
 
-Para la realización de la aplicación, se han utilizado 3 bases de datos distintas:
+Las bases de datos utilizadas para entrenar cada modelo son:
 - CBIS-DDDSM: Disponible en https://wiki.cancerimagingarchive.net/display/Public/CBIS-DDSM
 - INBreast: Disponible en https://www.kaggle.com/martholi/inbreast?select=inbreast.tgz
 - MIAS: Disponible en https://www.kaggle.com/kmader/mias-mammography/version/3?select=Info.txt
 
-Este repositorio pretende servir de base para que otras personas puedan aprovechar el trabajo realizado
-y unirse en la causa para la lucha contra el cáncer de seno. De este modo, a continuación se detallará la estructura
+**Este repositorio pretende servir de base para que otras personas puedan aprovechar el trabajo realizado
+y unirse en la causa para la lucha contra el cáncer de seno.** De este modo, a continuación se detallará la estructura
 del repositorio así como los objetivos de cada módulo o paquete.
 
 - `bin`: Contiene los archivos necesarios para crear una interfaz gráfica a partir de la librería `Pyqt5`. Entre estos
@@ -28,23 +28,31 @@ de la combinación secuencial de clasificadores).
     **En este módulo se deberían de añadir todas aquellas arquitecturas de red nuevas a incluir**. Por otra parte,
     también existen scripts para la creación secuencial de clasificadores a partir de un _Random Forest_. La generación
     de nuevos algorítmos podría introducirse en este script. 
+    
     - `breast_cancer_dataset`: Módulo que contiene los scripts utilizados para realizar el procesado de datos de 
     cada set de datos individual (CBIS, MIAS e INBreast). Estos scripts se encuentran en el paquete `databases` del 
     módulo, de modo que **para cualquier base de datos que se desee añadir, será necesario introducir su procesado en este
     paquete**. Por otra parte, el script _database_generator.py_ crea el set de datos utilizado por los algorítmos de 
     _deep learning_ utilizados uniendo cada base de datos individual contenida en el paquete `databases`. 
     Asimismo, se aplican técnicas de _data augmentation_ y se realiza el split de datos en entrenamiento y validación.
+    
      - `data_viz`: módulo utilizado para generar visualizaciones de los resultados obtenidos por las redes.
+     
      - `preprocessing`: módulo que contiene las funciones de preprocesado genéricas aplicadas a todos los conjuntos de 
      datos. Además, contiene las funcionalidades necesarias para estandarizar las imágenes a formato _png_ o _jpg_.
      **Cualquier procesado nuevo a añadir, deberá hacerse en este módulo**.
+     
      - `static`: módulo que contiene los archivos estáticos utilizados para la creación de la interfaz gráfica del 
      programa como ficheros _.css_, _.html_ e imágenes.
+     
      - `user_interace`:  módulo utilizado para crear la aplicación `Pyqt5` de clasificación de imágenes de seno.
+     
      - `utils`: módulo genérico en el cual configurar las rutas de las bases de datos dentro del entorno local desde 
      dónde se esté ejecutando el aplicativo, así como la configuración de los hiperparámetros de las redes neuronales. 
+     
      - `main_train.py`: script utilizado para realizar generar el pipeline de entrenamiento, desde la obtención de datos
      hasta la creación y el entrenamiento de cada modelo.
+     
      - `main.py`: script utilizado para lanzar la aplicación final realizada.
      
 Juntamente con los módulos contenidos en esta descripción, se crearán un conjunto de carpetas adicionales. Estas carpetas
@@ -53,7 +61,9 @@ sus objetivos:
 
 - `logging`: Carpeta que contendrá los logs de ejecuciones del programa, como por ejemplo los errores producidos durante
 el procesado de las imagenes.
+
 - `models`: Carpeta que contendrá los modelos almacenados juntamente con las predicciones realizadas durante el entrenamiento. 
+
 - `data`: Carpeta que contendrá las imagenes de cada set de datos convertidas (sub-directorio _01_CONVERTED_) y 
 procesadas (sub-directorio _02_PROCESED_). Esta carpeta tiene el objetivo de reiterar el proceso de procesado de imagenes
 una vez realizado.
--- src/algorithms/utils.py
+++ src/algorithms/utils.py
@@ -4,13 +4,12 @@ import numpy as np
 import utils.config as conf
 
 from multiprocessing import Queue
-from typing import Union, io, Callable
+from typing import Union, io
 from sklearn.metrics import roc_curve
 from sklearn.utils import resample
 
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing.image import Iterator
-from tensorflow import one_hot
 from tensorflow.keras import Model
 from tensorflow.keras.backend import argmax
 from tensorflow.keras.callbacks import CSVLogger, EarlyStopping, ReduceLROnPlateau
@@ -27,16 +26,13 @@ def get_predictions(keras_model: models, data: Iterator, **kwargs) -> pd.DataFra
 
     :param keras_model: modelo sobre el que se aplicará el método .predict para generar las predicciones
     :param data: dataset sobre el cual aplicar las predicciones
-    :param class_labels: diccionario con las clases verdaderas del set de datos. Este diccionario debe contener como
-                         key la posición correspondiente a cada prediccion dentro del vector de probabilidades devuelto
-                         por el método predict() de keras y, como value, el nombre de la clase
     :param kwargs: columnas adicionales para añadir al dataframe devuelto. El key de kwargs será el nombre de la columna
                    y los values serán el valor asignado a cada columna.
     :return: dataframe con el path de la imagen, la clase verdadera (en caso de existir), la clase predicha y columnas
              definidas por kwargs.
     """
 
-    # Se genera un orden aleatorio del datagenerator
+    # Se genera un orden aleatorio del datagenerator producido por on_epoch-end
     data.on_epoch_end()
 
     # se recupera el path de los archivos del dataset generator
@@ -47,13 +43,12 @@ def get_predictions(keras_model: models, data: Iterator, **kwargs) -> pd.DataFra
     if hasattr(data, 'classes'):
         true_labels = [data.classes[i] for i in data.indexes]
 
-    # Se predicen los datos. Debido a que la salida del modelo para cada muestra es un vector de probabilidades,
-    # dada por la función de activacón softmax, se obtiene la clase predicha mediante el valor máximo (clase más
-    # probable).
+    # Se define el tamaño de batch a 1 para predecir todas las observaciones
     data.set_batch_size(1)
+    # Se predicen los datos y se recupera la probabilidad de la clase 1 (maligno)
     predictions = keras_model.predict(data)[:, 1]
 
-    # Se crea el dataset final
+    # Se crea el dataset final con los ficheros, prediccion y clase real en caso de existir
     if true_labels:
         dataset = pd.DataFrame({'PROCESSED_IMG': fnames, 'PREDICTION': predictions, 'IMG_LABEL': true_labels})
     else:
@@ -69,33 +64,34 @@ def get_predictions(keras_model: models, data: Iterator, **kwargs) -> pd.DataFra
 def training_pipe(m: Model, db: BreastCancerDataset, q: Queue, c: conf.MODEL_FILES, task_type: str, fc: str = 'simple',
                   weight_init: Union[str, io] = None, frozen_layers: Union[str, int] = None) -> None:
     """
-    Función utilizada para generar el pipeline de entrenamiento de cada modelo.
+    Función utilizada para generar el pipeline de entrenamiento de cada modelo. Dado que tensorflow no libera cache
+    al entrenar un modelo, se debe de llamar esta función a través de un thread o proceso paralelo.
 
-    :param q:
-    :param c:
-    :param df: objeto dataset con el cual obtener los dataframe iterators de entrenamiento y validacion.
-    :param model_name: nombre del modelo para seleccionar qué modelo entrenar
-    :param bs: tamaño del batch
-    :param n_epochs: número de épocas
-    :param name: nombre del test para almacenar el modelo y los logs generados
-    :param opt: optimizador de gradiente descendiente a utilizar durante el proceso de back propagation
-    :param queue: queue para devolver los resultados al proceso principal
+    :param m: Red neuronal (objeto de la clase General Model) que contendrá cada algoritmo de dl
+    :param db: Objeto BreastCancerDataset con las observaciones de los conjuntos de entrenamiento y validacion
+    :param q: Queue para transmitir comunicar el resultado al thread principal.
+    :param c: objeto Model files que contiene información sobre ls rutas de guardado de cada modelo
+    :param task_type: admite los valores 'classification' o 'segmentation' para escoger el tipo de tarea a realizar
+    :param weight_init: nombre o path de los pesos con los que inicializar el entrenamiento de un modelo.
+    :param frozen_layers: número de capas a entrenar en cada modelo
 
     """
     # Se inicializa cada modelo:
     cnn = m(n=len(db.class_dict), weights=None if weight_init == 'random' else weight_init, top_fc=fc)
 
-    # Se registran las métricas que se desean almacenar:
+    # Se registran las métricas que se desean almacenar y se obtienen los conjuntos de train y validacion aplicando
+    # el escalado propio de cada red y la función de preprocesado propia. El tamaño de batch se define a partir
+    # de la hoja de configuraciones.
     if task_type == 'classification':
         cnn.register_metric(*list(conf.CLASSIFICATION_METRICS.values()))
-        #
-        # train, val = db.get_classification_dataset_generator(
-        #     batch_size=cnn.BS_DICT[frozen_layers], callback=cnn.get_preprocessing_func(), size=cnn.shape[:2]
-        # )
 
         train, val = db.get_classification_dataset_generator(
-            batch_size=conf.BATCH_SIZE, callback=cnn.get_preprocessing_func(), size=cnn.shape[:2]
+            batch_size=cnn.BS_DICT[frozen_layers], callback=cnn.get_preprocessing_func(), size=cnn.shape[:2]
         )
+
+        # train, val = db.get_classification_dataset_generator(
+        #     batch_size=conf.BATCH_SIZE, callback=cnn.get_preprocessing_func(), size=cnn.shape[:2]
+        # )
 
     elif task_type == 'segmentation':
         cnn.register_metric(*list(conf.SEGMENTATION_METRICS.values()))
@@ -107,25 +103,29 @@ def training_pipe(m: Model, db: BreastCancerDataset, q: Queue, c: conf.MODEL_FIL
     else:
         raise ValueError(f'task_type not incorporated')
 
-    # Queue que servirá para recuparar las predicciones de cada modelo.
     name = cnn.__name__
     if frozen_layers != 'ALL':
         filename = f'{name}_FineTunning'
     else:
         filename = f'{name}_Scratch'
 
-    # Se registran los callbacks del modelo:
+    # Se registran los callbacks del modelo (Earlystopping y CSV logger del train de cada modelo)
     csv_filepath = get_path(c.model_log_dir, weight_init, frozen_layers, f'{filename}.csv')
     cnn.register_callback(
         early_stopping=EarlyStopping(monitor='val_loss', mode='min', patience=20, restore_best_weights=True),
         log=CSVLogger(filename=csv_filepath, separator=';', append=True)
     )
-
+    # En función de las capas congeladas y la inicializacion de los pesos se realiza el entrenamiento de cada
+    # arquitectura siguiendo la siguiente lógica:
+    #     - Si se entrenan todas las capas, se llama al metodo train_from_scratch.
+    #     - En caso contrario, primero se entrenan únicamente las capas FC de cada modelo y posteriormente se entrena
+    #       el modelo con las capas especificadas por el usuario.
+    # por defecto el optimizador es Adam haciendo uso del learning rate identificado en la hoja de config.
     if frozen_layers == 'ALL':
         print(f'{"=" * 75}\nEntrenando {name} desde 0 con inicialización de pesos {weight_init}\n{"=" * 75}')
-
+        # Entrenamiento desde 0
         t, e = cnn.train_from_scratch(train, val, conf.EPOCHS, Adam(conf.LEARNING_RATE))
-
+        # Se registra el etrenamiento del modelo (tiempo, capas, inicialización) en un csv
         bulk_data(file=c.model_summary_train_csv, mode='a', cnn=name, process='Scratch', FT=frozen_layers,
                   weights=weight_init, time=t, epochs=e, trainable_layers=cnn.get_trainable_layers())
         print(f'{"=" * 75}\nEntrenamiento finalizado.\n{"=" * 75}')
@@ -135,27 +135,29 @@ def training_pipe(m: Model, db: BreastCancerDataset, q: Queue, c: conf.MODEL_FIL
               f'{weight_init}. Número de capas a entrenar {frozen_layers}\n{"=" * 75}')
 
         print(f'{"-" * 75}\n\tEmpieza proceso de extract-features (warm up)\n{"-" * 75}')
-
+        # Se realiza el extract features para entrenar los pesos de la capa FC
         t, e = cnn.extract_features(train, val, conf.WARM_UP_EPOCHS, Adam(conf.LEARNING_RATE))
-
+        # Se registra el etrenamiento del modelo (tiempo, capas, inicialización) en un csv
         bulk_data(file=c.model_summary_train_csv, mode='a', cnn=name, process='ExtractFeatures', FT=frozen_layers,
                   weights=weight_init, time=t, epochs=e, trainable_layers=cnn.get_trainable_layers())
         print(f'{"-" * 75}\n\tEntrenamiento finalizado.\n{"-" * 75}')
 
         print(f'{"-" * 75}\n\tEmpieza proceso de fine-tunning\n{"-" * 75}')
-
+        # Se entrena el modelo congelando las capas especificadas por el usuario
         t, e = cnn.fine_tunning(train, val, conf.EPOCHS, Adam(cnn.get_learning_rate()), frozen_layers)
-
+        # Se registra el etrenamiento del modelo (tiempo, capas, inicialización) en un csv
         bulk_data(file=c.model_summary_train_csv, mode='a', cnn=name, process='FineTunning', FT=frozen_layers,
                   weights=weight_init, time=t, epochs=e, trainable_layers=cnn.get_trainable_layers())
         print(f'{"-" * 75}\n\tEntrenamiento finalizado.\n{"-" * 75}')
 
         print(f'{"=" * 75}\nProceso de transfer learning finalizado\n{"=" * 75}')
 
+    # Se almacenan los pesos del modelo
     print(f'{"=" * 75}\nAlmacenando  modelo.\n{"=" * 75}')
     cnn.save_weights(dirname=get_path(c.model_store_cnn_dir, weight_init, frozen_layers), model_name=f"{name}.h5")
     print(f'{"=" * 75}\nModelo almacenado correctamente.\n{"=" * 75}')
 
+    # En el caso de realizar una clasificación, se obtienen las predicciones de cada instancia.
     if task_type == 'classification':
 
         print(f'{"=" * 75}\nObteniendo predicciones del modelo {name}.\n{"=" * 75}')
@@ -175,7 +177,13 @@ def training_pipe(m: Model, db: BreastCancerDataset, q: Queue, c: conf.MODEL_FIL
         q.put(True)
 
 
-def optimize_threshold(true_labels: np.array, pred_labels: np.array):
+def optimize_threshold(true_labels: np.array, pred_labels: np.array) -> float:
+    """
+    Función utilizada para obtimizar el threshold a partir del estadístico J de Youden (maximizar tasa de tpr y tnr).
+    :param true_labels: Vector con las clases verdaderas
+    :param pred_labels: Vector con las clases predichas
+    :return: threshold que maximiza la diferencia entre tpr y fpr = tpr + tnr - 1
+    """
 
     try:
         fpr, tpr, thresholds = roc_curve(true_labels, pred_labels)
@@ -186,9 +194,19 @@ def optimize_threshold(true_labels: np.array, pred_labels: np.array):
 
 
 def apply_bootstrap(data: pd.DataFrame, true_col: str, pred_col: str, metric: callable, iters: int = 1000,
-                    ci: float = 0.95, prop = 0.75, **kwargs) -> \
-        tuple:
-
+                    ci: float = 0.95, prop: float = 0.75, **kwargs) -> tuple:
+    """
+    Función utilizada para aplicar un bootstrap y obtener una métrica de actucación de un modelo.
+    :param data: pandas dataframe con los datos verdaderos y predichos de cada instancia
+    :param true_col: nombre de la columna del dataframe con los datos verdaderos
+    :param pred_col: nombre de la columna del dataframe con los datos predidchos
+    :param metric: callback sobre el cual aplicar la métrica
+    :param iters: número de iteraciones para realizar el algoritmo de bootstrap
+    :param ci: interalo de confianza para obtener la metrica
+    :param prop: proporción del set de datos a tener en cuenta para aplicar el bootstrap
+    :param kwargs: parámetros del callable metric
+    :return: media del intervalo con sus respectivos limites (mínimo y máximo).
+    """
     assert true_col in data.columns, f'{true_col} not in dataframe'
     assert pred_col in data.columns, f'{pred_col} not in dataframe'
 
